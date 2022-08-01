1ec5f36109d1ea26d82c164fdec68a788dde8eec
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-08-01 20:36:17 +0600

    PP Added

---
+++ Signal.js
@@ -0,0 +1,15 @@
+ var traficlightColor = "Yellow";
+ var traficlightColor = "Red";
+ var traficlightColor = "Green";
+
+ var lightStatus = "Green";
+
+ if(lightStatus == "Green"){
+    console.log('You can Go Now');
+ }
+ if(lightStatus == "Yellow"){
+    console.log('Just Slow Your Car Dwon Now');
+ }
+ if(lightStatus == "Red"){
+    console.log('Stop!!! You Cant go now.');
+ }
