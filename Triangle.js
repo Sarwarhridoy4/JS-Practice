330e9a977f07632c2d79b7f015f0c42fb5dd0b07
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-08-01 14:06:05 +0600

    logic added

--- Triangle.js
+++ Triangle.js
@@ -2,7 +2,7 @@ var sideA = 9;
 var sideB = 8;
 var sideC = 9;
 
-if(sideA == sideB ||sideA == sideC){
+if(sideA == sideB ||sideA == sideC || sideB ==sideC){
     console.log('Triangle is Isosceles')
 }
 else{
