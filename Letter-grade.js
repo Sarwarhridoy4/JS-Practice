f3f9704c42e7fc954943324c551e5b36d8ee78c9
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-08-01 12:38:13 +0600

    2nd problem solved

---
+++ Letter-grade.js
@@ -0,0 +1,31 @@
+// Set Score as array
+score = [85 , 66 , 95 , 56 , 40];
+var myScore = score[0];
+var tomsScore = score[1]
+var janisScore = score[2]
+var petersScore = score[3]
+var johnsScore = score[4]
+
+// grades
+
+// Function to get the Grade.
+// my Result
+if (myScore >= 80 || myScore ==100) {
+    console.log('You Have Obtained:A');
+
+}
+else if (myScore >= 60 || myScore <=79) {
+    console.log('You Have Obtained:B');
+
+}
+else if (myScore >= 50 || myScore <=59) {
+    console.log('You Have Obtained:C');
+
+}
+else if (myScore == 40) {
+    console.log('You Have Obtained:D');
+}
+else {
+    console.log('Try Again Next Time')
+}
+
