85ff75178cc59b71b2cec246dcfc3813f24a8235
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-08-01 13:35:51 +0600

    third problem1 solved

---
+++ comparator.js
@@ -0,0 +1,13 @@
+var firstNumber = 13;
+var secondNumber = 79;
+var thirdNumber = 45;
+
+if(firstNumber < secondNumber){
+    console.log('Biggest Number is:',secondNumber);
+}
+else if(firstNumber < thirdNumber){
+    console.log('Biggest Number Is:',thirdNumber);
+}
+else{
+    console.log(firstNumber);
+}
