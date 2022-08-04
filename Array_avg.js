4e4e2ad8c7245e0b07e82f6b315b82eedb13302d
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-08-04 21:22:40 +0600

    Solved

--- Array_avg.js
+++ Array_avg.js
@@ -2,7 +2,7 @@ const array = [25, 39, 3, 40, 62];
 let sum = 0;
 let length = array.length; 
 
-function avarage(sum,length){
+function avarage(length){
     for (let i = 0; i < length; i++) {
         sum += array[i];
         avg = sum/length;
@@ -10,5 +10,5 @@ function avarage(sum,length){
     return avg;
 }
 
-let Avarage = avarage(avg);
+let Avarage = avarage(length);
 console.log('You Avarage is:',Avarage);
